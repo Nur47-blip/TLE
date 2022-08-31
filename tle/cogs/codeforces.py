cd39b37162b5eeaaef59b77d73de0ceb55efa46d
alam <muhammadalamsyah47@gmail.com>
2022-08-31 16:01:45 +0700

    change codeforces.py dan handles.py

--- tle/cogs/codeforces.py
+++ tle/cogs/codeforces.py
@@ -69,7 +69,7 @@ class Codeforces(commands.Cog):
         - Type ;upsolve for listing all available problems.
         - Type ;upsolve <nr> for choosing the problem <nr> as gitgud problem (only possible if you have no active gitgud challenge)
         - After solving the problem you can claim gitgud points for it with ;gotgud
-        - If you can't solve the problem for 2 hours you can skip it with ;nogud
+        - If you can't solve the problem for 1 hours you can skip it with ;nogud
         - The all-time ranklist can be found with ;gitgudders
         - A monthly ranklist is shown when you type ;monthlygitgudders
         - Another way to gather gitgud points is ;gitgud (only works if you have no active gitgud-Challenge)
@@ -77,7 +77,7 @@ class Codeforces(commands.Cog):
         
         Point distribution:
         delta  | -300 | -200 | -100 |  0  | +100 | +200 | +300 | +400 | +500 | +600 | +700
-        points |   2  |   3  |   5  |  8  |  12  |  17  |  23  |  23  |  23  |  23  |  23
+        points |   2  |   3  |   5  |  8  |  12  |  17  |  23  |  30  |  38  |  47  |  57
         """
         handle, = await cf_common.resolve_handles(ctx, self.converter, ('!' + str(ctx.author),))
         user = cf_common.user_db.fetch_cf_user(handle)
@@ -270,7 +270,7 @@ class Codeforces(commands.Cog):
         """Gitgud: You can request a problem from the bots relative to your current rating with ;gitgud <delta>
         - It is also possible to request problems with a certain tag now but you get less points for it: ;gitgud <delta> [+tags...] [~tags...]
         - After solving the problem you can claim gitgud points for it with ;gotgud
-        - If you can't solve the problem for 2 hours you can skip it with ;nogud
+        - If you can't solve the problem for 1 hours you can skip it with ;nogud
         - The all-time ranklist can be found with ;gitgudders
         - A monthly ranklist is shown when you type ;monthlygitgudders
         - Another way to gather gitgud points is ;upsolve (only works if you have no active gitgud-Challenge)
@@ -278,8 +278,8 @@ class Codeforces(commands.Cog):
         
         Point distribution:
         delta  | -300 | -200 | -100 |  0  | +100 | +200 | +300 | +400 | +500 | +600 | +700
-        no tags|   2  |   3  |   5  |  8  |  12  |  17  |  23  |  23  |  23  |  23  |  23
-        tags   |   2  |   2  |   2  |  3  |   5  |   8  |  12  |  17  |  23  |  23  |  23
+        no tags|   2  |   3  |   5  |  8  |  12  |  17  |  23  |  30  |  38  |  47  |  57
+        tags   |   2  |   2  |   2  |  3  |   5  |   8  |  12  |  17  |  23  |  30  |  38
         """
         handle, = await cf_common.resolve_handles(ctx, self.converter, ('!' + str(ctx.author),))
         user = cf_common.user_db.fetch_cf_user(handle)
